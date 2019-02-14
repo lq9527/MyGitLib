ac46e0ab9ab4cb7e3ee8b95ac642e45c746df4ad
liqiang32 <liqiang32@58ganji.com>
2019-02-14 15:37:39 +0800

    change22

--- app/src/main/java/com/lq/mygitlib/MainActivity.java
+++ app/src/main/java/com/lq/mygitlib/MainActivity.java
@@ -3,7 +3,6 @@ package com.lq.mygitlib;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.lq.lib_helloworld.HelloWorld;
 import com.lq.lib_helloworld2.Hello2;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,9 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        new HelloWorld().say();
 
         new Hello2().say2();
     }
